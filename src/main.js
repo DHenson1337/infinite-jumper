0bddc6f0999cc9656ebb5b295976a62a6d7c87a5
dhenson <davonhensontech@gmail.com>
2025-02-09 19:17:18 -0500

    First push for Phaser game infinite Jumper created in phaser v4

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Level from "./scenes/Level.js";
+import Preload from "./scenes/Preload.js";
+
+window.addEventListener('load', function () {
+
+	var game = new Phaser.Game({
+		width: 1280,
+		height: 720,
+		type: Phaser.AUTO,
+        backgroundColor: "#242424",
+		scale: {
+			mode: Phaser.Scale.FIT,
+			autoCenter: Phaser.Scale.CENTER_BOTH
+		}
+	});
+
+	game.scene.add("Preload", Preload, true);
+	game.scene.add("Level", Level);
+	game.scene.add("Boot", Boot, true);
+});
